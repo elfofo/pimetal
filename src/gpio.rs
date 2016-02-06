2eb702a352c8193c3468761c3f104ab4096713a2
El Fofo <therealegyptien@gmail.com>
2016-02-06 07:09:54 +0100

    gpio module

---
+++ src/gpio.rs
@@ -0,0 +1,45 @@
+
+struct Pin {
+	pub num: u32,
+	fun_sel: *mut u32,
+	bit_sel: u32,
+	fun_set: *mut u32,
+	fun_clr: *mut u32,
+	bit_set_clr: u32,
+}
+
+impl Pin {
+	pub fn new(pin: u32) -> Pin {
+		let base = GPIO_BASE as *mut u32;
+		let ipin = pin as isize;
+		Pin {
+			num: pin,
+			fun_sel: unsafe { base.offset(0x04 * (ipin/10)) as *mut u32 },
+			bit_sel: (pin % 10) * 3, 
+			fun_set: unsafe { base.offset(GPF_SET + (ipin/32)) as *mut u32 },
+			fun_clr: unsafe { base.offset(GPF_CLR + (ipin/32)) as *mut u32 },
+			bit_set_clr: pin % 32,
+		}
+	}
+	
+	pub fn as_out(&mut self) {
+		unsafe { *(self.fun_sel) |= 1 << self.bit_sel; }
+	}
+	pub fn as_in(&mut self) {
+		unsafe { *(self.fun_sel) |= 0 << self.bit_sel; }
+	}
+	pub fn on(&mut self) {
+		unsafe { *(self.fun_set) = 1 << self.bit_set_clr; }
+	}
+	pub fn off(&mut self) {
+		unsafe { *(self.fun_clr) = 1 << self.bit_set_clr; }
+	}
+	pub fn set(&mut self, is_on: bool) {
+		match is_on {
+			true => self.on(),
+			false => self.off(),
+		}
+	}
+}
+
+
